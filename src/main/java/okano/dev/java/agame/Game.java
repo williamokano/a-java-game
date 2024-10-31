599ec4378bc00e7d427d510e2d7434776f055de6
William Okano <williamokano@gmail.com>
2024-10-31 09:43:59 +0100

    feat: a rectangle is drawn 👀

--- src/main/java/okano/dev/java/agame/Game.java
+++ src/main/java/okano/dev/java/agame/Game.java
@@ -1,8 +1,12 @@
 package okano.dev.java.agame;
 
 public class Game {
+
     private GameWindow gameWindow;
+    private GamePanel gamePanel;
+
     public Game() {
-        this.gameWindow = new GameWindow();
+        this.gamePanel = new GamePanel();
+        this.gameWindow = new GameWindow(this.gamePanel);
     }
 }
--- src/main/java/okano/dev/java/agame/GameWindow.java
+++ src/main/java/okano/dev/java/agame/GameWindow.java
@@ -3,12 +3,17 @@ package okano.dev.java.agame;
 import javax.swing.*;
 
 public class GameWindow {
+
     private JFrame jFrame;
-    public GameWindow() {
+
+    public GameWindow(GamePanel gamePanel) {
         jFrame = new JFrame();
 
         jFrame.setSize(400, 400);
         jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        jFrame.add(gamePanel);
+
         jFrame.setVisible(true);
     }
 }
